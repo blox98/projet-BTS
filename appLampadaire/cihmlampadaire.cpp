51928c7c4f62a8df45336ce5e44e06b75a83a812
matthieu <35302770+blox98@users.noreply.github.com>
2018-01-29 11:46:31 +0100

    appLampadaire 0.2
    deuxième version du code avec une console qui affiche les changement effectuer et des boites de messages

--- appLampadaire/cihmlampadaire.cpp
+++ appLampadaire/cihmlampadaire.cpp
@@ -21,21 +21,28 @@ void CIhmLampadaire::on_PbLire_clicked()
     ui->LineDh->setText("0");
     ui->LineDl->setText("ffff");
     ui->LineNi->setText("Maître");
+    ui->txtConsole->setText("Les paramètres ont été mis à jour");
     QMessageBox::information(this,"Lecture","Les paramètres ont été mis à jour");
 }
 
 void CIhmLampadaire::on_PbEnregistrer_clicked()
 {
+    ui->txtConsole->setText("Les paramètres ont été enregistrer");
     QMessageBox::information(this,"Enregistrement","Les paramètres ont été enregistrer");
 }
 
-void CIhmLampadaire::on_pushButton_clicked()
+void CIhmLampadaire::on_pReset_clicked()
 {
-    ui->lineId->setText("");
-    ui->BoxJv->setCurrentText("Disabled [0]");
-    ui->BoxCe->setCurrentText("Disabled [0]");
-    ui->LineDh->setText("");
-    ui->LineDl->setText("");
-    ui->LineNi->setText("");
-    QMessageBox::information(this,"Reset"," Restauration des paramètres par défaut");
+    QMessageBox::StandardButton reply;
+      reply =  QMessageBox::question(this, "Attention", "Vous etes sur le point de restaurer les paramètres par défaut", QMessageBox::No|QMessageBox::Yes);
+      if (reply == QMessageBox::Yes) {
+        ui->lineId->setText("");
+        ui->BoxJv->setCurrentText("Disabled [0]");
+        ui->BoxCe->setCurrentText("Disabled [0]");
+        ui->LineDh->setText("");
+        ui->LineDl->setText("");
+        ui->LineNi->setText("");
+        ui->txtConsole->setText("Restauration des paramètres par défaut");
+        QMessageBox::information(this,"Reset"," Restauration des paramètres par défaut");
+      }
 }
--- appLampadaire/cihmlampadaire.h
+++ appLampadaire/cihmlampadaire.h
@@ -25,6 +25,8 @@ private slots:
 
     void on_pushButton_clicked();
 
+    void on_pReset_clicked();
+
 private:
     Ui::CIhmLampadaire *ui;
 };
